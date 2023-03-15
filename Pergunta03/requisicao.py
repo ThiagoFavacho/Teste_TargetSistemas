56d5a6dff3fa343354c5b2e1589f46d1ad2edab0
Thiago Favacho R <thiagofr89@hotmail.com>
2023-03-14 23:03:43 -0300

    Correção de código - Pergunta 03

--- Pergunta03/requisicao.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("dados.json") as dados_json:
-    dados = json.load(dados_json)
-
-print('Maior Faturamento', max(dados, key=lambda x: x['valor']))
-print('Menor Faturamento', min(dados, key=lambda x: x['valor']))
